5625c6e45d352dcf961c3d57bbc75fffa82ae5ec
Vitor Lucas <vitorluc.2013@gmail.com>
2020-09-14 22:09:49 -0300

    Project created

---
+++ backend/src/main/java/com/lucasvitor/pesquisagames/PesquisagamesApplication.java
@@ -0,0 +1,13 @@
+package com.lucasvitor.pesquisagames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisagamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisagamesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lucasvitor/pesquisagames/PesquisagamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasvitor.pesquisagames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisagamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
